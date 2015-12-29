011b02d564833c17cd76052a567262ffb0e59ff5
plasko_a <antoine.plaskowski@epitech.eu>
2015-12-29 22:59:20 +0100

    fix eliott noob

---
+++ UnitTestMyWindowsMediaPlayer/TestXmlBDD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWindowsMediaPlayer.Model;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestMyWindowsMediaPlayer
+{
+    [TestClass]
+    public class TestXmlBDD
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IBDD ibdd = new XmlBDD(Assembly.GetCallingAssembly().GetName().Name);
+
+            var mediaList = new List<String>();
+
+            foreach (var i in Enumerable.Range(0, 42))
+                mediaList.Add(i.ToString());
+            ibdd.AddMedia(mediaList, "the answer to life the universe and everything");
+        }
+    }
+}
